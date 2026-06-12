d02d86cf31fb2423947d38dd02cb433c96fbb361
Firegame224 <xg474627@gmail.com>
2026-06-13 01:57:12 +0700

    feat: add screeningService & screening route to handle screening feature

--- src/lib/types/screening.ts
+++ src/lib/types/screening.ts
@@ -1,7 +1,15 @@
+import { AppThemeEnum } from "./theme"
+
+export type ScreeningRequest = {
+    type: "ONBOARDING" | "DAILY",
+    answers: ScreeningAnswer[],
+}
+
 export type ScreeningResult = {
     type: "ONBOARDING" | "DAILY",
     score: number,
     answers: ScreeningAnswer[],
+    theme: AppThemeEnum
 }
 
 export type ScreeningAnswer = {
---
+++ src/lib/types/theme.ts
@@ -0,0 +1,9 @@
+
+export type AppTheme = "calm_blue" | "warm_yellow" | "alert_orange" | "deep_purple";
+
+export enum AppThemeEnum {
+    CALM_BLUE = "calm_blue",
+    WARM_YELLOW = "warm_yellow",
+    ALERT_ORANGE = "alert_orange",
+    DEEP_PURPLE = "deep_purple",
+}
